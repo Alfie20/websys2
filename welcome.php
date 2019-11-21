cfef23a0ab1a64dc6a6176d2d4cd1a4ec93baf25
Alfie20 <53362991+Alfie20@users.noreply.github.com>
2019-11-21 11:43:58 +0800

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -4,7 +4,9 @@
 		<style type="text/css">
 		body{
 			font-family: sans-serif;
-		}
+			margin: 0;
+			padding: 0;
+					}
 		button:hover {
 			background-color: green;
 			transition: 0.5s;
@@ -14,33 +16,82 @@
 			border: none;
 
 		}
-
-
+		.sidenav {
+			height: 100%;
+			width: 0;
+			position: absolute;
+			z-index: 1;
+			top:0;
+			left: 0;
+			background-color: green;
+			transition: 0.5s;
+			padding-top:60px;
+		}
+		.sidenav a{
+			padding: 8px 8px 8px 32px;
+			text-decoration: none;
+			font-size: 25px;
+			color: white;
+			display: block;
+			transition: 0.5px;
+		}
+		.sidenav a:hover .offcanvas a:focus{
+			color:green;
+		}
+		.closebtn{
+			position: absolute;
+			top:0;
+			right: 25px;
+			font-size: 36px !important;
+			margin-left: 50px;
+			color: green;
+		}
+		@media screen and (max-height: 450px;)
+		{
+			.sidenav {padding-top: 15px;}
+			.sidenav a{font-size: 18px;}
+		}
 		</style>
 
 
 	</head>
-<body>
+<body >
 
 <center>
+<header style="color: white; background-color: black;">
 <h1>
 	<?php echo "My name is Alfie Villarin";?>
 </h1>
-
+</header>
 	<a style="text-decoration: none; color: black;" href="index.php"> <button>home</button></a>
-	<h3 style="font-family: sans-serif;">This is my work</h3>
-<div  style="border-radius: 10px; box-shadow: 1px 1px 10px black;  border:1px solid black; text-decoration: none;height: 200px; width: 200px;">
-	<a href="condition.php"><button style="margin-top: 10px;">Condition</button></a></br>
-	<a href="loop.php"><button>Loop</button></a></br>
-	<a href="post.php"><button>Post</button></a></br>
-	<a href="string.php"><button>String</button></a></br>
-	<a href="wspace.php"><button>Wspace</button></a></br>
-	<a href="getmethod.php"><button>Getmethod</button></a></br>
-	<a href="string.php"><button>String</button></a></br>
-	<a href="get.php"><button>Get</button></a>
+
+
+
+
+
+<div id="mySidenav" class="sidenav">
+	<a href="javascript:void(0)" class="closebtn" onclick="closeNav()">X</a>
+	<a href="condition.php">Condition</a></br>
+	<a href="loop.php">Loop</a></br>
+	<a href="post.php">Post</a></br>
+	<a href="string.php">String</a></br>
+	<a href="wspace.php">Wspace</a></br>
+	<a href="getmethod.php">Getmethod</a></br>
+	<a href="string.php">String</a></br>
+	<a href="get.php">Get</a>
 
 </div>
-
+<div>
+<span style="font-size: 30px; cursor: pointer; margin-top: 300px;position: absolute;margin-left: -150px;" onclick="openNav()">open here for my works</span>
+</div>
+<script>
+function openNav(){
+	document.getElementById("mySidenav").style.width="250px";
+}	
+function closeNav(){
+	document.getElementById("mySidenav").style.width="0";
+}
+</script>
 </center>
 	</body>
 </html>
